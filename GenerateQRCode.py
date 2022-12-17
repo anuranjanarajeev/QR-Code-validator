5ee59a540de37a724aac6d5c9e434ccd4f74d9e1
navyasraj02 <navyasraj2003@gmail.com>
2022-12-18 02:36:35 +0530

    COMMIT 3 - Edited flask app,Added result.html

--- GenerateQRCode.py
+++
@@ -1,3 +0,0 @@
-import pyqrcode
-qr = pyqrcode.create("HORN O.K. PLEASE.")
-qr.png("horn.png", scale=6)
--- app.py
+++ app.py
@@ -1,18 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
+from pyzbar.pyzbar import decode
+from PIL import Image
+import os
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
-qrcodeData = "sdwajk"
+
+app.config['SECRET_KEY'] = 'supersecretkey'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = 'sample/files'
 
 
-@app.route('/')
-def hello_world():
+def DecodeQRCode(qrcodeimg):  # pass path of img
+    decocdeQR2 = decode(Image.open(qrcodeimg))
+    if (decocdeQR2):
+        a = "VALID QR CODE"
+        # data=decocdeQR2[0].data.decode('ascii')
+    else:
+        a = "INVALID QR CODE"
+    return a
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
     return render_template("home.html")
 
 
-@app.route('/login')
-def login():
-    return render_template("login.html")
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+
+@app.route('/upload', methods=['GET', "POST"])
+def upload_files():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data  # First grab the file
+        s = file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                   app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))  # Then save the file
+        filename = secure_filename(file.filename)
+        userFile = "sample\\files\\"+filename
+        result = DecodeQRCode(userFile)
+        return render_template("result.html", result=result)
+
+    return render_template('upload.html', form=form)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True)
